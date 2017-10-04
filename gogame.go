7cdaca88f43696993155e6a0084a8f6f7c75f6c9
unknown <vandenbosch@BA_INFO33.CHBAH.ORG>
2017-10-04 17:26:49 +0200

    dev

--- gogame.go
+++ gogame.go
@@ -1,10 +1,37 @@
 package gogame
 
-import "github.com/veandco/go-sdl2/sdl"
+//import "github.com/veandco/go-sdl2/sdl"
+
+//delta
+var DeltaTime float32
+
+//game
+//type Game struct {
+//	deltaTime float32
+//
+
+//hjjkh
+//type GameFunc func()
+
+//jkjk
+type Game interface {
+	Start()
+	Update()
+}
+
+type GameFixedUpdate interface {
+	FixedUpdate()
+}
 
 //run
-func Run() {
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+func Run(g Game) {
+	g.Start()
+	g.Update()
+	if c, ok := g.(GameFixedUpdate); ok {
+		c.FixedUpdate()
+	}
+
+	/*if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
 	defer sdl.Quit()
@@ -25,5 +52,5 @@ func Run() {
 	surface.FillRect(&rect, 0xffff0000)
 	window.UpdateSurface()
 
-	sdl.Delay(2500)
+	sdl.Delay(2500)*/
 }
