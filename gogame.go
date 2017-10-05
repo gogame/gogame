ef5ded30cb964e0a76f3d7440ffe2a8be4c15632
unknown <vandenbosch@BA_INFO33.CHBAH.ORG>
2017-10-05 15:29:04 +0200

    dev

--- gogame.go
+++ gogame.go
@@ -1,5 +1,7 @@
 package gogame
 
+import "fmt"
+
 //import "github.com/veandco/go-sdl2/sdl"
 
 //delta
@@ -14,19 +16,93 @@ var DeltaTime float32
 //type GameFunc func()
 
 //jkjk
-type Game interface {
+/*type Game interface {
 	//Start()
 	Update()
 }
 
 type GameFixedUpdate interface {
 	FixedUpdate()
+}*/
+
+//
+type Starter interface {
+	Start()
 }
 
+//
+type Updater interface {
+	Update()
+}
+
+//u
+type UpdaterFunc func()
+
+//a
+func (f UpdaterFunc) Update() {
+	if f != nil {
+		f()
+	}
+}
+
+//a
+type Game struct {
+	Start       func()
+	Update      Updater
+	FixedUpdate func()
+}
+
+//a
+type StarterFunc func()
+
+//a
+func (f StarterFunc) Start() {
+	f()
+}
+
+//a
+
+//todo: renomer methods en updater
+/*func Run(u GameUpdateFunc) {
+	g := &Game{Methods: u}
+	g.Run()
+}*/
+
+// Run is a simple method when only Start and Update are needed
+func Run(start, update func()) {
+	g := &Game{Start: start, Update: UpdaterFunc(update)}
+	g.Run()
+}
+
+/*
+//
+func Run(u Updater) {
+	g := &Game{Updater: u}
+	g.Run()
+}
+
+//
+func RunFunc(u func()) {
+	Run(UpdaterFunc(u))
+}
+*/
+//
+
 //run
-func Run(g Game) {
+func (g *Game) Run() {
+
 	//g.Start()
-	g.Update()
+	if g.Start != nil {
+		g.Start()
+	}
+	fmt.Printf("%v %T\n", g.Update, g.Update)
+	if g.Update != nil {
+		fmt.Println("not nil")
+		g.Update()
+	} else {
+		fmt.Println("nil")
+	}
+
 	/*if c, ok := g.(GameFixedUpdate); ok {
 		c.FixedUpdate()
 	}
