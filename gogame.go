f4af65e70f3681881873d436fda24e8b376aaad1
bombjackm <marc.vandenbosch@gmail.com>
2020-12-17 17:03:53 +0100

    dev

--- gogame.go
+++ gogame.go
@@ -1,5 +1,45 @@
 package gogame
 
-func Run() {
-	println("running")
+type Game interface {
+	Update()
+	//Graphics graphics.Device
+	//Width uint
+	/*, Height uint
+	Title         string*/
+}
+
+/*
+func (x *Game) Init() {
+	println("GOGAME init")
+}*/
+
+type GameTime struct {
+	t float32
+}
+
+type Initializable interface {
+	Init()
+}
+
+type Updatable interface {
+	Update()
+}
+
+type Drawable interface {
+	Draw()
+}
+
+func Run(game Game) {
+
+	init, ok := game.(Initializable)
+	println("init: ", init, ok)
+	if init != nil {
+		init.Init()
+	}
+
+	upd, ok := game.(Updatable)
+	println("update:", upd, ok)
+	if upd != nil {
+		upd.Update()
+	}
 }
